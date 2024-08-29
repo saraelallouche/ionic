b6cb4f12a6f658e1a4cfa599fa0d6f163294e9be
saraelallouche <saraelallouche@hotmail.fr>
2024-08-29 18:55:14 +0200

    last version

--- accessVision/src/app/app.module.ts
+++ accessVision/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
-
+import { Platform } from '@ionic/angular';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [BluetoothSerial, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [Platform, AndroidPermissions, BluetoothSerial, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- accessVision/src/app/tab1/tab1.page.ts
+++ accessVision/src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 import { BluetoothStatusService } from '../service/bluetooth-status.service';
+import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial/ngx';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -10,23 +11,26 @@ export class Tab1Page {
 
 
  constructor(
-    private bluetoothSerial: BluetoothSerial,
-    private bluetoothStatusService: BluetoothStatusService
+    private bluetoothSerial: BluetoothSerial
   ) {}
 
+
   connectToDevice() {
-    this.bluetoothSerial.connect('D8:3A:DD:94:E4:06').subscribe(
-      () => {
-        alert('Connected to raspberry pi!');
-        console.log('Connected to raspberry pi!');
-        this.bluetoothStatusService.updateStatus('connected');
-      },
-      (error) => {
-        alert('Error connecting to raspberry pi!');
-        console.error('Connection error', error);
-        this.bluetoothStatusService.updateStatus('disconnected');
+   this.bluetoothSerial.list().then((devices) => {
+      console.log('Available devices:', devices);
+      const device = devices.find((d: any) => d.id === 'D8:3A:DD:94:E4:06');
+      if (device) {
+        this.bluetoothSerial.connect(device.id).subscribe(
+          success => console.log('Connected successfully to', device.name),
+          error => console.error('Connection failed', error)
+        );
+      } else {
+        console.error('Could not find the peripheral with the specified address.');
       }
-    );
+    }, (error) => {
+      console.error('Error listing Bluetooth devices', error);
+    });
+
   }
 
   listenForNotifications() {
