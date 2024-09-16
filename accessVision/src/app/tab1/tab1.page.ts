8ce2d6ca6f78efe828d5af2b7e71ab03d7da4f4e
Victor de Vitry <devitryv@gmail.com>
2024-09-16 20:39:28 +0200

    plus besoin de rentrer l'adresse mac, simplification des pages et amélioration des alertes vocales

--- accessVision/src/app/tab1/tab1.page.ts
+++ accessVision/src/app/tab1/tab1.page.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import {BluetoothSerial} from "@ionic-native/bluetooth-serial/ngx";
-
-import {AndroidPermissions} from "@ionic-native/android-permissions/ngx";
-import {AlertController, NavController} from '@ionic/angular';
-import {Storage} from "@ionic/storage-angular";
-import {InAppBrowser} from "@ionic-native/in-app-browser/ngx";
-import {TextToSpeechService} from "../service/text-to-speech.service";
+import { BluetoothSerial } from "@ionic-native/bluetooth-serial/ngx";
+import { AndroidPermissions } from "@ionic-native/android-permissions/ngx";
+import { AlertController, NavController } from '@ionic/angular';
+import { Storage } from "@ionic/storage-angular";
+import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
+import { TextToSpeechService } from "../service/text-to-speech.service";
 
 @Component({
   selector: 'app-tab1',
@@ -16,7 +15,7 @@ export class Tab1Page {
 
   adresse: string = 'test';
 
- constructor(
+  constructor(
     private bluetoothSerial: BluetoothSerial,
     private androidPermissions: AndroidPermissions,
     private navCtrl: NavController,
@@ -24,19 +23,13 @@ export class Tab1Page {
     private alertController: AlertController,
     private iab: InAppBrowser,
     private ttsService: TextToSpeechService
+  ) { }
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.speak("Bienvenue sur l'application Access Vision");
+    }, 3000);
 
-  ) {}
-
-  async ngOnInit() {
-   await this.storage.create();
-    const storedAdresse = await this.storage.get('adresse_mac');
-    if (storedAdresse) {
-      this.speak("Bienvenue sur l'application Access Vision. Veuillez cliquer sur 'Se Connecter' pour commencer.")
-    }
-    else {
-      this.speak("Bienvenue sur l'application Access Vision. Veuillez saisir l'adresse MAC de votre appareil pour commencer.")
-    }
   }
 
   async ionViewDidEnter() {
@@ -51,6 +44,7 @@ export class Tab1Page {
     }).catch((error) => {
       console.error('Permissions refusées', error);
     });
+
   }
 
   async checkBluetoothPermissions(): Promise<void> {
@@ -66,20 +60,10 @@ export class Tab1Page {
   }
 
   async connectToDevice() {
-    let haveAdresse = await this.verifyAdresse();
-    if (!haveAdresse) {
-      return;
-    }
-    this.bluetoothSerial.connect(this.adresse).subscribe(
-      (data) => {
-        this.sendNotification('start');
-        this.navCtrl.navigateForward('/tabs/tab2 ');
-
-      },
-      (error) => {
-        console.error('Error connecting:', error);
-      }
-    );
+    this.findBluetoothDevice();
+    /*
+    
+    */
   }
 
   sendNotification(message: string) {
@@ -110,13 +94,39 @@ export class Tab1Page {
 
   speak(message: string) {
     this.ttsService.speak(message)
-      .then(() => console.log('Alerte lu avec succès'))
+      .then()
       .catch((error) => console.error('Erreur lors de la lecture du texte', error));
   }
 
-  // Fonction pour ouvrir le lien YouTube
   openTutorial() {
-    const url = 'https://www.youtube.com/channel/UCJXSJ07RDcSQSBFf9Sn_hOw'; // Lien vers la vidéo YouTube
-    this.iab.create(url, '_system'); // Ouvre le lien dans l'application ou le navigateur
+    const url = 'https://www.youtube.com/channel/UCJXSJ07RDcSQSBFf9Sn_hOw';
+    this.iab.create(url, '_system');
   }
+
+  findBluetoothDevice() {
+    this.speak("Connection au boitier");
+
+    this.bluetoothSerial.discoverUnpaired().then((devices) => {
+      let foundDevice = devices.find((device: { name: string | string[]; }) => device.name && device.name.includes("raspberry"));  
+      if (foundDevice) {
+        console.log(`Raspberry Pi trouvée: ${foundDevice.name} - MAC: ${foundDevice.id}`);
+        this.bluetoothSerial.connect(foundDevice.id).subscribe(
+          (data) => {
+            this.sendNotification('start');
+            this.speak("Vous êtes connecté");
+            this.navCtrl.navigateForward('/tabs/tab2');
+          },
+          (error) => {
+            console.error('Error connecting:', error);
+          }
+        );
+      } else {
+        console.log('Raspberry Pi non trouvée');
+      }
+    }).catch((error) => {
+      console.error('Erreur lors du scan Bluetooth', error);
+    });
+  };
 }
+
+
--- accessVision/src/app/tab2/tab2.page.ts
+++ accessVision/src/app/tab2/tab2.page.ts
@@ -10,24 +10,26 @@ import { TextToSpeechService } from '../service/text-to-speech.service';
 })
 export class Tab2Page implements OnInit {
 
+  // Flag pour vérifier si une lecture est en cours
+  private isSpeaking: boolean = false;
 
   constructor(
     private bluetoothStatusService: BluetoothStatusService,
     private bluetoothSerial: BluetoothSerial,
     private ttsService: TextToSpeechService
-
   ) {}
 
   ngOnInit() {
-    this.listenForMessages()
+    this.listenForMessages();
   }
 
   listenForMessages() {
     this.bluetoothSerial.isConnected().then(() => {
       this.bluetoothSerial.subscribe('\n').subscribe(
         (message: string) => {
-          console.log("Message reçu: "+message);
-          this.speak(this.cleanTensorString(message))
+          console.log("Message reçu: " + message);
+          // Tentative de lire le message seulement si aucune lecture n'est en cours
+          this.speak(this.cleanTensorString(message));
         },
         (error) => {
           console.error('Erreur lors de la réception de l alerte:', error);
@@ -39,9 +41,26 @@ export class Tab2Page implements OnInit {
   }
 
   speak(message: string) {
+    // Si une lecture est déjà en cours, on ne fait rien
+    if (this.isSpeaking) {
+      console.log('Lecture déjà en cours, alerte ignorée: ', message);
+      return;
+    }
+
+    // Marque la lecture comme en cours
+    this.isSpeaking = true;
+
     this.ttsService.speak(message)
-      .then(() => console.log('Alerte lu avec succès'))
-      .catch((error) => console.error('Erreur lors de la lecture du texte', error));
+      .then(() => {
+        console.log('Alerte lue avec succès');
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la lecture du texte', error);
+      })
+      .finally(() => {
+        // Remet le flag à false une fois la lecture terminée
+        this.isSpeaking = false;
+      });
   }
 
   cleanTensorString(message: string): string {
@@ -50,10 +69,10 @@ export class Tab2Page implements OnInit {
 
   disconnect() {
     this.bluetoothSerial.disconnect().then(() => {
+      this.speak("Vous êtes déconnecté du boitier");
       console.log('Bluetooth déconnecté avec succès');
     }).catch((error) => {
       console.error('Erreur lors de la déconnexion Bluetooth:', error);
     });
   }
-
 }
